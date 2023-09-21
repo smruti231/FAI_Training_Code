95d10efbd3fc7b4761ce0f546e7b2d864c76bce8
SMRUTI RANJAN POLAI <112563197+smruti231@users.noreply.github.com>
2023-09-22 05:29:54 +0530

    commit

---
+++ ASSIGNMENT & TODOs & Extras/Employee Management ASP .NET MVC CORE/Proj3MvcCoreApp/Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proj3MvcCoreApp.Models
+    {
+    [Table("EmpTable2")]
+    public class Employee
+        {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Address is mandatory")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Salary is must")]
+        [Range(10000, 50000, ErrorMessage = "Salary should be b/w 10000 to 50000")]
+        public int Salary { get; set; }
+        public int DeptId {  get; set; }
+
+        }
+    [Table("Department")]
+    public class  Dept
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DeptName { get; set; } = string.Empty;
+    }
+    public class MyDbContext : DbContext
+        {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+            { 
+
+            }
+        public DbSet<Employee> EmpTable2s { get; set;}
+        public DbSet<Dept> Departments { get; set;}
+        }
+}
---
+++ ASSIGNMENT & TODOs & Extras/Employee Management ASP .NET MVC CORE/Proj3MvcCoreApp/Models/IDBComponent.cs
@@ -0,0 +1,79 @@
+﻿namespace Proj3MvcCoreApp.Models
+    {
+    public interface IDBComponent
+        {
+        List<Employee> GetAllEmployees();
+        void AddEmployee (Employee employee);
+        void DeleteEmployee (int id);
+        void UpdateEmployee (Employee employee);
+        Employee GetEmployee(int id);
+        List<Dept> GetAllDepts();
+        Dept GetDept(int id);
+        }
+
+    public class DbComponent : IDBComponent
+        {
+        private readonly MyDbContext _dbContext;
+        public DbComponent(MyDbContext context)
+            {
+            _dbContext = context;
+            }
+
+        public void AddEmployee(Employee employee)
+            {
+            _dbContext.EmpTable2s.Add(employee);
+            _dbContext.SaveChanges();
+            }
+
+        public void DeleteEmployee(int id)
+            {
+            var selected = _dbContext.EmpTable2s.Find(id);
+            if (selected != null)
+                {
+                _dbContext.EmpTable2s.Remove(selected);
+                _dbContext.SaveChanges ();
+                }
+            else
+                {
+                throw new Exception("No REcord Found to delete");
+                }
+        }
+
+        public List<Dept> GetAllDepts()
+            {
+                return _dbContext.Departments.ToList();
+            }
+
+        public List<Employee> GetAllEmployees()
+            {
+            return _dbContext.EmpTable2s.ToList();
+            }
+
+        public Dept GetDept(int id)
+            {
+            return _dbContext.Departments.Find(id) ?? throw new Exception("Dept not found");
+            }
+
+        public Employee GetEmployee(int id)
+            {
+            return _dbContext.EmpTable2s.Find(id) ?? throw new Exception("Employee Not found");
+            }
+
+        public void UpdateEmployee(Employee employee)
+            {
+            var emp = _dbContext.EmpTable2s.Find(employee.Id);
+            if(emp != null)
+                {
+                emp.Salary = employee.Salary;
+                emp.Address = employee.Address;
+                emp.Name = employee.Name;
+                emp.DeptId = employee.DeptId;
+                _dbContext.SaveChanges();
+                }
+            else
+                {
+                throw new Exception("Employee not found to update");
+                }
+            }
+        }
+    }
